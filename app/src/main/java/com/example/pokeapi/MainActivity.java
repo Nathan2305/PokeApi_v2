e97344b7775e9b511b36a457a17723a62084018d
Nathan2305 <jonathan_20092411@hotmail.com>
2019-05-25 22:30:44 -0500

    Update MainActivity.java
    PokeApi terminado

--- app/src/main/java/com/example/pokeapi/MainActivity.java
+++ app/src/main/java/com/example/pokeapi/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.ybq.android.spinkit.style.Circle;
 import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
@@ -18,10 +20,15 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    Button buscar;
+    Button buscar, limpiar;
     TextInputEditText pokemon_name;
-    TextView nombrePk_txt,heightPk_txt,weightPk_txt;
+    TextView nombrePk_txt, heightPk_txt, weightPk_txt;
     ImageView fotoPk;
+    ProgressBar pbar;
+    String num = "";
+    Circle circle;
+    PokeApiInterface pokeApiInterface = null;
+    String namePok = "";
     private static final String PHOTO_POK = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
     @Override
@@ -31,64 +38,93 @@ public class MainActivity extends AppCompatActivity {
         buscar = findViewById(R.id.buscar);
         pokemon_name = findViewById(R.id.pokemon_name);
         nombrePk_txt = findViewById(R.id.nombrePk_txt);
-        heightPk_txt=findViewById(R.id.heightPk_txt);
-        weightPk_txt=findViewById(R.id.weightPk_txt);
+        heightPk_txt = findViewById(R.id.heightPk_txt);
+        weightPk_txt = findViewById(R.id.weightPk_txt);
         fotoPk = findViewById(R.id.fotoPk);
+        limpiar = findViewById(R.id.limpiar);
+        pbar = findViewById(R.id.pbar);
+        circle = new Circle();
+        pbar.setProgressDrawable(circle);
         buscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://pokeapi.co/api/v2/")
-                        .addConverterFactory(GsonConverterFactory.create())  //Como se va a formatear la respuesta JSON
-                        .build();
-                final PokeApiInterface pokeApiInterface = retrofit.create(PokeApiInterface.class);
-                Call<PokemonRespuesta> respuestaCall = pokeApiInterface.obtenerPokemonRespuesta();
-                respuestaCall.enqueue(new Callback<PokemonRespuesta>() {
-                    @Override
-                    public void onResponse(Call<PokemonRespuesta> call, Response<PokemonRespuesta> response) {
-                        if (response.isSuccessful()) {
-                            String namePok = pokemon_name.getText().toString();
-                            if (namePok != null || !"".equalsIgnoreCase(namePok)) {
-                                PokemonRespuesta pokemonRespuesta = response.body();
-                                for (Pokemon aux_pok : pokemonRespuesta.getResults()) {
-                                    if (namePok.equalsIgnoreCase(aux_pok.getName())) {
-                                        nombrePk_txt.setText(aux_pok.getName());
-                                        String[] numberPok = aux_pok.getUrl().split("/");
-                                        final String num = numberPok[numberPok.length - 1];
-                                        Call<PokemonDetalle> pokemonDetalleCall=pokeApiInterface.obteberPokemonDetalla(num);
-                                        pokemonDetalleCall.enqueue(new Callback<PokemonDetalle>() {
-                                            @Override
-                                            public void onResponse(Call<PokemonDetalle> call, Response<PokemonDetalle> response) {
-                                                if (response.isSuccessful()){
-                                                    PokemonDetalle pokemonDetalle=response.body();
-                                                    weightPk_txt.setText(pokemonDetalle.getWeight());
-                                                    heightPk_txt.setText(pokemonDetalle.getHeight());
-                                                    Picasso.with(getApplicationContext()).load(PHOTO_POK + num + ".png").into(fotoPk);
-                                                }
+                namePok = pokemon_name.getText().toString();
+                if (!namePok.isEmpty()) {
+                    pbar.setVisibility(View.VISIBLE);
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("https://pokeapi.co/api/v2/")
+                            .addConverterFactory(GsonConverterFactory.create())  //Como se va a formatear la respuesta JSON
+                            .build();
+                    pokeApiInterface = retrofit.create(PokeApiInterface.class);
+                    Call<PokemonRespuesta> respuestaCall = pokeApiInterface.obtenerPokemonRespuesta("300"); // 300 primeros
+                    respuestaCall.enqueue(new Callback<PokemonRespuesta>() {
+                        @Override
+                        public void onResponse(Call<PokemonRespuesta> call, Response<PokemonRespuesta> response) {
+                            PokemonRespuesta pokemonRespuesta = response.body();
+                            boolean existsPokemon = false;
+                            for (Pokemon aux_pok : pokemonRespuesta.getResults()) {
+                                if (namePok.equalsIgnoreCase(aux_pok.getName())) {
+                                    existsPokemon = true;
+                                    String[] numberPok = aux_pok.getUrl().split("/");
+                                    num = numberPok[numberPok.length - 1];
+                                    Call<PokemonDetalle> pokemonDetalleCall = pokeApiInterface.obteberPokemonDetalla(num);
+                                    pokemonDetalleCall.enqueue(new Callback<PokemonDetalle>() {
+                                        @Override
+                                        public void onResponse(Call<PokemonDetalle> call, Response<PokemonDetalle> response) {
+                                            if (response.isSuccessful()) {
+                                                PokemonDetalle pokemonDetalle = response.body();
+                                                nombrePk_txt.setText(namePok);
+                                                weightPk_txt.setText(pokemonDetalle.getWeight());
+                                                heightPk_txt.setText(pokemonDetalle.getHeight());
+                                                Picasso.with(getApplicationContext()).load(PHOTO_POK + num + ".png").into(fotoPk);
+                                                pbar.setVisibility(View.GONE);
                                             }
+                                        }
 
-                                            @Override
-                                            public void onFailure(Call<PokemonDetalle> call, Throwable throwable) {
-
-                                            }
-                                        });
-                                        //Picasso.with(getApplicationContext()).load(PHOTO_POK + num + ".png").into(fotoPk);
-                                        break;
-                                    }
+                                        @Override
+                                        public void onFailure(Call<PokemonDetalle> call, Throwable throwable) {
+                                        }
+                                    });
+                                }
+                                if (existsPokemon) {
+                                    break;
                                 }
                             }
-                        }else{
-                            Toast.makeText(getApplicationContext(),"Algo salio mal "+ response.getClass().toString(),Toast.LENGTH_LONG).show();
+                            if (!existsPokemon) {
+                                Toast.makeText(getApplicationContext(), "No existe el pokemon " + namePok, Toast.LENGTH_SHORT).show();
+                                cleanViews();
+                                pbar.setVisibility(View.GONE);
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<PokemonRespuesta> call, Throwable throwable) {
 
                         }
-                    }
-
-                    @Override
-                    public void onFailure(Call<PokemonRespuesta> call, Throwable throwable) {
-                        Toast.makeText(getApplicationContext(),"Algo salio mal "+ throwable.getMessage(),Toast.LENGTH_LONG).show();
-                    }
-                });
+                    });
+                }
             }
         });
+        limpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanViews();
+            }
+        });
+
+    }
+
+    public void cleanViews() {
+        pokemon_name.setText("");
+        nombrePk_txt.setText("");
+        weightPk_txt.setText("");
+        heightPk_txt.setText("");
+        fotoPk.setImageResource(android.R.color.transparent);
+        pokemon_name.requestFocus();
     }
 }
+
+
+
+
+
